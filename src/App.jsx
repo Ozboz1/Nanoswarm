3f9cba4d247cd0da0de43fda35e701a444133e9e
Ozboz1 <ozgan.cakir@gmail.com>
2025-06-14 19:28:30 +0100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+// src/App.jsx
+import React from "react";
+import GameCanvas from "./components/GameCanvas";
+
+export default function App() {
+  return (
+    <div>
+      <GameCanvas />
+    </div>
+  );
+}
